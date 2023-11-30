5bc0f1cd29ef397a678d50a07e4dba7f1b6fd272
Tylar Hiss <tylar.hiss@amadeus.com>
2023-11-30 09:47:56 -0700

    add quick rules

---
+++ unleash-2023/rules.ts
@@ -0,0 +1,21 @@
+
+// order:
+
+// if we're lower than 10 battery, wait, turn off light
+
+// if were higher than 2500, go down, unless flagged to return
+
+// explore fish horizontally until no fish/ until flagged to return
+
+// return (set flag to return)
+
+
+
+
+
+
+// needs:
+
+
+// way of flagging return
+
--- unleash-2023/unleash-2023.ts
+++ unleash-2023/unleash-2023.ts
@@ -282,60 +282,17 @@ const gameState = new GameState();
 gameState.readCreatureCount();
 
 // game loop
-while (true) {
-  //   const myScore: number = parseInt(readline());
-  //   const foeScore: number = parseInt(readline());
-  //   const myScanCount: number = parseInt(readline());
-  //   for (let i = 0; i < myScanCount; i++) {
-  //     const creatureId: number = parseInt(readline());
-  //   }
-  //   const foeScanCount: number = parseInt(readline());
-  //   for (let i = 0; i < foeScanCount; i++) {
-  //     const creatureId: number = parseInt(readline());
-  //   }
-  //   const myDroneCount: number = parseInt(readline());
-  //   for (let i = 0; i < myDroneCount; i++) {
-  //     var inputs: string[] = readline().split(" ");
-  //     const droneId: number = parseInt(inputs[0]);
-  //     const droneX: number = parseInt(inputs[1]);
-  //     const droneY: number = parseInt(inputs[2]);
-  //     const emergency: number = parseInt(inputs[3]);
-  //     const battery: number = parseInt(inputs[4]);
-  //   }
-  //   const foeDroneCount: number = parseInt(readline());
-  //   for (let i = 0; i < foeDroneCount; i++) {
-  //     var inputs: string[] = readline().split(" ");
-  //     const droneId: number = parseInt(inputs[0]);
-  //     const droneX: number = parseInt(inputs[1]);
-  //     const droneY: number = parseInt(inputs[2]);
-  //     const emergency: number = parseInt(inputs[3]);
-  //     const battery: number = parseInt(inputs[4]);
-  //   }
-  //   const droneScanCount: number = parseInt(readline());
-  //   for (let i = 0; i < droneScanCount; i++) {
-  //     var inputs: string[] = readline().split(" ");
-  //     const droneId: number = parseInt(inputs[0]);
-  //     const creatureId: number = parseInt(inputs[1]);
-  //   }
-  //   const visibleCreatureCount: number = parseInt(readline());
-  //   for (let i = 0; i < visibleCreatureCount; i++) {
-  //     var inputs: string[] = readline().split(" ");
-  //     const creatureId: number = parseInt(inputs[0]);
-  //     const creatureX: number = parseInt(inputs[1]);
-  //     const creatureY: number = parseInt(inputs[2]);
-  //     const creatureVx: number = parseInt(inputs[3]);
-  //     const creatureVy: number = parseInt(inputs[4]);
-  //   }
-  //   const radarBlipCount: number = parseInt(readline());
-  //   for (let i = 0; i < radarBlipCount; i++) {
-  //     var inputs: string[] = readline().split(" ");
-  //     const droneId: number = parseInt(inputs[0]);
-  //     const creatureId: number = parseInt(inputs[1]);
-  //     const radar: string = inputs[2];
-  //   }
+while (true) {  
   gameState.readGameState();
   printErr(JSON.stringify(gameState));
   for (let i = 0; i < gameState.myDroneCount; i++) {
+
+    var drone = gameState.myDrones[i];
+
+    // if we're higher than 
+
+
+
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
 
