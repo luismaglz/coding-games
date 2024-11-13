435ef64afea412933f320d9238a747178aedb006
Luis Gonzalez <luismaglz@live.com>
2024-11-13 09:45:17 -0700

    updateds

---
+++ unleash-2024/initial.ts
@@ -0,0 +1,40 @@
+// /**
+//  * Grow and multiply your organisms to end up larger than your opponent.
+//  **/
+
+// var inputs: string[] = readline().split(" ");
+// const width: number = parseInt(inputs[0]); // columns in the game grid
+// const height: number = parseInt(inputs[1]); // rows in the game grid
+
+// // game loop
+// while (true) {
+//   const entityCount: number = parseInt(readline());
+//   for (let i = 0; i < entityCount; i++) {
+//     var inputs: string[] = readline().split(" ");
+//     const x: number = parseInt(inputs[0]);
+//     const y: number = parseInt(inputs[1]); // grid coordinate
+//     const type: string = inputs[2]; // WALL, ROOT, BASIC, TENTACLE, HARVESTER, SPORER, A, B, C, D
+//     const owner: number = parseInt(inputs[3]); // 1 if your organ, 0 if enemy organ, -1 if neither
+//     const organId: number = parseInt(inputs[4]); // id of this entity if it's an organ, 0 otherwise
+//     const organDir: string = inputs[5]; // N,E,S,W or X if not an organ
+//     const organParentId: number = parseInt(inputs[6]);
+//     const organRootId: number = parseInt(inputs[7]);
+//   }
+//   var inputs: string[] = readline().split(" ");
+//   const myA: number = parseInt(inputs[0]);
+//   const myB: number = parseInt(inputs[1]);
+//   const myC: number = parseInt(inputs[2]);
+//   const myD: number = parseInt(inputs[3]); // your protein stock
+//   var inputs: string[] = readline().split(" ");
+//   const oppA: number = parseInt(inputs[0]);
+//   const oppB: number = parseInt(inputs[1]);
+//   const oppC: number = parseInt(inputs[2]);
+//   const oppD: number = parseInt(inputs[3]); // opponent's protein stock
+//   const requiredActionsCount: number = parseInt(readline()); // your number of organisms, output an action for each one in any order
+//   for (let i = 0; i < requiredActionsCount; i++) {
+//     // Write an action using console.log()
+//     // To debug: console.error('Debug messages...');
+
+//     console.log("WAIT");
+//   }
+// }
--- unleash-2024/unleash-2023.ts
+++ unleash-2024/unleash-2023.ts
@@ -2,208 +2,219 @@ declare function readline(): string;
 declare function print(value: string): void;
 declare function printErr(message: string): void;
 
+type EntityTypes =
+  | "WALL"
+  | "ROOT"
+  | "BASIC"
+  | "TENTACLE"
+  | "HARVESTER"
+  | "SPORER"
+  | "A"
+  | "B"
+  | "C"
+  | "D";
+
+type EntityLetter = "A" | "B" | "C" | "D";
+
+class Entity {
+  x: number;
+  y: number;
+  type: EntityTypes;
+  owner: number;
+  organId: number;
+  organDir: string;
+  organParentId: number;
+  organRootId: number;
+}
+
 class GameState {
-  turns: number = 0;
+  width: number;
+  height: number;
 
-  constructor() {}
+  entities: Entity[] = [];
 
-  log(): void {
-    // debug(`radarBlips ${JSON.stringify(this.radarBlips)}`);
+  myA: number;
+  myB: number;
+  myC: number;
+  myD: number;
+
+  oppA: number;
+  oppB: number;
+  oppC: number;
+  oppD: number;
+
+  requiredActionsCount: number;
+
+  turns = 0;
+
+  constructor(height: number, width: number) {
+    this.width = width;
+    this.height = height;
   }
 
-  checkCustomGameState() {
+  updateGameState() {
+    const entityCount: number = parseInt(readline());
+    for (let i = 0; i < entityCount; i++) {
+      var inputs: string[] = readline().split(" ");
+      const x: number = parseInt(inputs[0]);
+      const y: number = parseInt(inputs[1]); // grid coordinate
+      const type: string = inputs[2]; // WALL, ROOT, BASIC, TENTACLE, HARVESTER, SPORER, A, B, C, D
+      const owner: number = parseInt(inputs[3]); // 1 if your organ, 0 if enemy organ, -1 if neither
+      const organId: number = parseInt(inputs[4]); // id of this entity if it's an organ, 0 otherwise
+      const organDir: string = inputs[5]; // N,E,S,W or X if not an organ
+      const organParentId: number = parseInt(inputs[6]);
+      const organRootId: number = parseInt(inputs[7]);
 
-  }
-
-
-  readGameState() {
-    // read and update gamestate
-
-
-    this.turns++;
-  }
-}
-
-class Player {
-
-  droneActions: Action[] = [
-    // new BailIfMonster(),
-   
-  ];
-
-  constructor(
-    droneId: number,
-    droneX: number,
-    droneY: number,
-    emergency: number,
-    battery: number
-  ) {
-  
-  }
-
-  wait(light: boolean, message: string = "") {
-    console.log(`WAIT ${light ? 1 : 0} ${message}`);
-  }
-
-  move(x: number, y: number, light: boolean, message: string = "") {
-    console.log(`MOVE ${x} ${y} ${light ? 1 : 0} ${message}`);
-  }
-
-  debugPosition() {
-    debug(`Drone ${this.droneId} is at ${this.droneX}, ${this.droneY}`);
-  }
-}
-
-
-
-abstract class Action {
-  completed: boolean;
-
-  constructor() {
-    this.completed = false;
-  }
-
-  abstract runAction(
-    drone: Player,
-    gameState: GameState,
-    action: Action1
-  ): boolean;
-}
-
-
-
-class Action1 extends Action {
-  constructor() {
-    super();
-  }
-
-  runAction(
-    drone: Player,
-    gameState: GameState,
-    droneAction: DroneActionLol
-  ): boolean {
-    return true;
-  }
-}
-
-class DoZone3Action extends Action {
-  constructor() {
-    super();
-  }
-
-  runAction(
-    drone: Player,
-    gameState: GameState,
-    droneAction: DroneActionLol
-  ): boolean {
-    debug("DoZone3Action");
-
-    var unscannedZone3Fish = gameState.getZ3UnscannedCreatures();
-
-    if (unscannedZone3Fish.length === 0) {
-      this.completed = true;
-      return false;
+      const entity = new Entity();
+      entity.x = x;
+      entity.y = y;
+      entity.type = type;
+      entity.owner = owner;
+      entity.organId = organId;
+      entity.organDir = organDir;
+      entity.organParentId = organParentId;
+      entity.organRootId = organRootId;
+      this.entities.push(entity);
     }
 
-    var firstFish = unscannedZone3Fish[0];
-    gameState.targetFish.push(firstFish);
+    var inputs: string[] = readline().split(" ");
+    this.myA = parseInt(inputs[0]);
+    this.myB = parseInt(inputs[1]);
+    this.myC = parseInt(inputs[2]);
+    this.myD = parseInt(inputs[3]); // your protein stock
 
-    var loc = gameState.radarBlips.find((r) => r.creatureId === firstFish);
+    var inputs: string[] = readline().split(" ");
+    this.oppA = parseInt(inputs[0]);
+    this.oppB = parseInt(inputs[1]);
+    this.oppC = parseInt(inputs[2]);
+    this.oppD = parseInt(inputs[3]); // opponent's protein stock
 
-    var radarLoc = loc?.radar;
+    this.requiredActionsCount = parseInt(readline()); // your number of organisms, output an action for each one in any order
+  }
 
-    if (radarLoc === "TL") {
-      droneAction.targetLocation.x = drone.droneX - 600;
-      droneAction.targetLocation.y = drone.droneY - 600;
+  findClosestLetter(organ: Entity): Entity {
+    const letters = this.entities.filter((e) => {
+      e.type === "A" || e.type === "B" || e.type === "C" || e.type === "D";
+    });
 
-      return true;
-    } else if (radarLoc === "TR") {
-      droneAction.targetLocation.x = drone.droneX + 600;
-      droneAction.targetLocation.y = drone.droneY - 600;
+    const distanceArray = letters.map((l) => {
+      return distanceArray(organ.x, organ.y, l.x, l.y);
+    });
 
-      return true;
-    } else if (radarLoc === "BL") {
-      droneAction.targetLocation.x = drone.droneX - 600;
-      droneAction.targetLocation.y = drone.droneY + 600;
+    const min = Math.min(distanceArray);
 
-      return true;
-    } else if (radarLoc === "BR") {
-      droneAction.targetLocation.x = drone.droneX + 600;
-      droneAction.targetLocation.y = drone.droneY + 600;
+    const index = distanceArray.findIndex(min);
 
-      return true;
+    return letters[index];
+  }
+
+  goToClosestLetterAndOrgan(): { organ: Entity; letter: Entity } {
+    const organ = this.getRightMostEntity();
+    const letter = this.findClosestLetter(organ);
+
+    return {
+      organ,
+      letter,
+    };
+  }
+
+  getRightMostEntity(): Entity {
+    // get the entity with the rightmost position
+
+    const myEnts = this.entities.filter((e) => {
+      return e.owner === 1;
+    });
+
+    let entity: Entity | null = null;
+
+    myEnts.forEach((e) => {
+      if (entity === null) {
+        entity = e;
+      }
+
+      if (entity.x <= e.x) {
+        entity = e;
+      }
+    });
+
+    return entity!;
+  }
+}
+
+type Direction = "LEFT" | "RIGHT" | "TOP" | "BOTTOM";
+type EntityType = "BASIC" | "ROOT";
+
+function distance(x1: number, y1: number, x2: number, y2: number): number {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function moveEntity(entity: Entity, direction: Direction, type: EntityType) {
+  let x = entity.x;
+  let y = entity.y;
+
+  switch (direction) {
+    case "LEFT": {
+      x = x - 1;
+      break;
     }
-
-    droneAction.wait = true;
-
-    return true;
-  }
-}
-
-class GameBoard {
-  minX: number = 1;
-  minY: number = 1;
-  maxX: number = 9999;
-  maxY: number = 9999;
-}
-
-
-class TurnOffLightIfLowBattery extends Action {
-  constructor() {
-    super();
-  }
-
-  runAction(
-    drone: Player,
-    gameState: GameState,
-    action: DroneActionLol
-  ): boolean {
-    if (drone.battery < 10) {
-      action.light = false;
+    case "BOTTOM": {
+      y = y - 1;
+      break;
     }
-
-    return false;
-  }
-}
-
-class MoveTo extends Action {
-  constructor(
-    public x: number,
-    public y: number
-  ) {
-    super();
-  }
-
-  runAction(
-    drone: Player,
-    gameState: GameState,
-    action: DroneActionLol
-  ): boolean {
-    if (
-      Math.abs(drone.droneX - this.x) < 300 &&
-      Math.abs(drone.droneY - this.y) < 300
-    ) {
-      this.completed = true;
-      return false;
+    case "RIGHT": {
+      x = x + 1;
+      break;
     }
+    case "TOP": {
+      y = y + 1;
+      break;
+    }
+  }
 
-    action.targetLocation.x = this.x;
-    action.targetLocation.y = this.y;
-    return true;
+  console.log(`GROW ${entity.organId} ${x} ${y} ${type}`);
+}
+
+function moveToLetter(organ: Entity, letter: Entity) {
+  if (organ.x < letter.x) {
+    return moveEntity(organ, "RIGHT", "BASIC");
+  }
+
+  if (organ.x > letter.x) {
+    return moveEntity(organ, "LEFT", "BASIC");
+  }
+
+  if (organ.y < letter.y) {
+    return moveEntity(organ, "BOTTOM", "BASIC");
+  }
+
+  if (organ.x > letter.x) {
+    return moveEntity(organ, "TOP", "BASIC");
   }
 }
 
-const gameState = new GameState();
+/**
+ * Grow and multiply your organisms to end up larger than your opponent.
+ **/
+
+var inputs: string[] = readline().split(" ");
+const width: number = parseInt(inputs[0]); // columns in the game grid
+const height: number = parseInt(inputs[1]); // rows in the game grid
+
+const gameState = new GameState(height, width);
 
 // game loop
 while (true) {
-  gameState.readGameState();
-  gameState.log();
-  for (let i = 0; i < gameState.myDrones.length; i++) {
+  // Read all the game inputs
+  gameState.updateGameState();
 
+  for (let i = 0; i < gameState.requiredActionsCount; i++) {
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+    const { organ, letter } = gameState.goToClosestLetterAndOrgan();
+
+    moveToLetter(organ, letter);
+
+    // console.log("WAIT" + gameState.requiredActionsCount);
   }
-}
-
-function debug(message: string) {
-  printErr(message);
 }
